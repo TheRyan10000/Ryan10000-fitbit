7fa604d123197f624b3d0692f42a01bc713fa620
TheRyan10000 <49083357+TheRyan10000@users.noreply.github.com>
2024-08-28 23:17:09 -0500

    initial commit

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+
+Config.notifytimems = 60000
+Config.notifyhunger = 25
+Config.notifythirst = 25
+Config.notifystress = 75
+Config.item = "fitbit"
+
+Config.audio =
+{
+    set = "HUD_AWARDS",
+    name = "CHALLENGE_UNLOCKED",
+}
